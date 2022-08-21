0e7ab9e985926331bdc229a50f734747f65e5827
Agami-Malikov <agami.malikov@gmail.com>
2022-08-21 15:54:14 +0300

    Update ContactList.jsx

--- src/components/Phonebook/ContactList/ContactList.jsx
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-// import s from './contactList.module.css';
+import PropTypes from 'prop-types';
 
 const ContactList = ({ contacts, removeContact }) => {
   const elements = contacts.map(({ id, name, number }) => (
@@ -11,7 +11,22 @@ const ContactList = ({ contacts, removeContact }) => {
       </li>
     </>
   ));
+  console.log(elements);
   return <ul>{elements}</ul>;
 };
 
 export default ContactList;
+
+ContactList.defaultProps = {
+  contacts: [],
+};
+
+ContactList.prototypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.func.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
